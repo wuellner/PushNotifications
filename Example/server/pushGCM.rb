14018a19438e3b49c3288f5b92bf95e1e3d95aa1
Eevert Saukkokoski <eevert.saukkokoski@appgyver.com>
2014-12-23 16:13:30 +0200

    more self evident constants to replace

--- Example/server/pushGCM.rb
+++ Example/server/pushGCM.rb
@@ -1,9 +1,13 @@
 require 'rubygems'
 require 'pushmeup'
+
+APPLICATION_API_KEY = "API_KEY_GOES_HERE"
+DEVICE_REGISTRATION_ID = "REGISTRATION_ID_GOES_HERE"
+
 GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
-GCM.key = "API_KEY_GOES_HERE"
-destination = ["REGISTRATION_ID_GOES_HERE"]
+GCM.key = APPLICATION_API_KEY
+destination = [DEVICE_REGISTRATION_ID]
 data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
 
 puts GCM.send_notification( destination, data)
