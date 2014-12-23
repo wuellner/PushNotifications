a875e6810acc8e814e637886178b888c78d10217
Eevert Saukkokoski <eevert.saukkokoski@appgyver.com>
2014-12-23 16:10:24 +0200

    show JSON result from sending notification

--- Example/server/pushGCM.rb
+++ Example/server/pushGCM.rb
@@ -6,4 +6,4 @@ GCM.key = "API_KEY_GOES_HERE"
 destination = ["REGISTRATION_ID_GOES_HERE"]
 data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
 
-GCM.send_notification( destination, data)
+puts GCM.send_notification( destination, data)
